04cbc9ac28f7a593c794988c94a1e27475128848
eneuwirt <eduard.neuwirt@gmail.com>
2025-12-13 05:10:41 +0100

    Simplify UserEditView layout by removing HTML components

--- src/main/java/com/vaaskel/Application.java
+++ src/main/java/com/vaaskel/Application.java
@@ -1,6 +1,6 @@
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  *
@@ -9,13 +9,14 @@ package com.vaaskel;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Theme(value = "my-app")
+@Theme(value = "vaaskel", variant = Lumo.DARK)
 public class Application implements AppShellConfigurator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
